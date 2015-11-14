20c812311526c33f6dbce27bf77c439033e97b2a
Matej Kollar <208115@mail.muni.cz>
2015-11-14 13:47:47 +0100

    Re-think tagging script

--- build-tools/tag.sh
+++ build-tools/tag.sh
@@ -3,23 +3,31 @@ set -eu
 declare -r project=aeson-parsec-picky
 declare -r cabalfile=${project}.cabal
 
-if [ ${#} -ne 1 ]; then
-    echo 'Usage: tag.sh <next-version>'
+function die() {
+    echo "${@}"
     exit 1
-fi
+}
 
-if ! [ -f "${cabalfile}" ]; then
-    echo "Script need to be run in directory where is ${cabalfile}"
-    exit 1
-fi
+function main() {
+    if [ ${#} -ne 0 ]; then
+        die 'Usage: tag.sh'
+    fi
 
-declare -r new_version=${1}
-declare -r current_version=$( sed -n 's/^version:\s*//p' "${cabalfile}" )
+    if ! [ -f "${cabalfile}" ]; then
+        die "Script need to be run in directory where is ${cabalfile}"
+    fi
 
-sed -i '/^source-repository this$/,${s/\(tag:\s*\).*/\1v'"${current_version}"'/}' "${cabalfile}"
+    declare -r last_version=$( sed -n '/^source-repository this$/,/^$/{s/^\s\+tag:\s*v//p}' "${cabalfile}" )
+    declare -r current_version=$( sed -n 's/^version:\s*//p' "${cabalfile}" )
 
-cabal sdist
+    if [[ ${current_version} == ${last_version} ]]; then
+        die -e "ERROR: Version have not changed since the last releas\nConsider version bump."
+    fi
 
-sed -i '{s/^\(version:\s*\).*/\1'"${new_version}"'/}' "${cabalfile}"
-git commit -m "Release version ${current_version} and bump latest to ${new_version}" "${cabalfile}"
-git tag "v${current_version}"
+    sed -i '/^source-repository this$/,/^$/{s/\(tag:\s*\).*/\1v'"${current_version}"'/}' "${cabalfile}"
+    git commit -m "Release version ${current_version}" "${cabalfile}"
+    git tag "v${current_version}"
+    cabal sdist
+}
+
+main "${@}"
