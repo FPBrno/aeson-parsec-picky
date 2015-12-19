3280258e800ebc349e2df77b51dcc5d42e2ed7ab
Matej Kollar <xkolla06@stud.fit.vutbr.cz>
2015-12-19 21:00:05 +0100

    Add checks for tagging/releasing script

--- build-tools/tag.sh
+++ build-tools/tag.sh
@@ -30,9 +30,22 @@ function main() {
     declare -r last_version=$( sed -n '/^source-repository this$/,/^$/{s/^\s\+tag:\s*v//p}' "${cabalfile}" )
     declare -r current_version=$( sed -n 's/^version:\s*//p' "${cabalfile}" )
 
+    echo "Checking whether there is something to change..."
     if [[ ${current_version} == ${last_version} ]]; then
         die -e "ERROR: Version have not changed since the last releas\nConsider version bump."
     fi
+
+    echo "Checking tags in git..."
+    if git tag | grep -q "^v${current_version}$"; then
+        die -e "ERROR: Version ${current_version} already have been released.\nConsider version bump."
+    fi
+
+    echo "Checking changelog..."
+    if ! grep -q "^Version ${current_version}$" CHANGES.md; then
+        die -e "ERROR: Missing information in changelog.\nConsider adding them."
+    fi
+
+    exit 1
 
     sed -i '/^source-repository this$/,/^$/{s/\(tag:\s*\).*/\1v'"${current_version}"'/}' "${cabalfile}"
     git commit -m "Release version ${current_version}" "${cabalfile}"
